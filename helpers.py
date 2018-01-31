6dcd6ab48d21d097404bf99b3f05c0df46fdf682
Milan S Doodeman <milandoodeman@gmail.com>
2018-01-31 08:15:11 +0000

    beetje opmaak changenamepass

--- helpers.py
+++ helpers.py
@@ -18,26 +18,35 @@ def login_required(f):
     return decorated_function
 
 def api_query(terms, length = 1000):
+
+    # set the parameters for the query
     payload = {'app_id' : 'abec09cd',
             'app_key' : '66cc31dcd04ab364bff95bd62fe527c8',
             'q' : terms,
             'to' : length
         }
+
+    # make connection with the API and return the query results
     return requests.get('http://api.edamam.com/search', params=payload).json()
 
 def searchfunction(results):
+
+    # get the images from the recipes
     imglink = []
     for hit in results['hits']:
         imglink.append(hit['recipe']['image'])
 
+    # get the recipe names
     names = []
     for hit in results['hits']:
         names.append(hit['recipe']['label'])
 
+    # get the links where the recipes come from
     urls = []
     for hit in results['hits']:
         urls.append(hit['recipe']['url'])
 
+    # make a dict per recipe and make a list of dicts
     reclist = []
     for i in range(len(imglink)):
         temp = {}
